5706050908010c0fd0150f9878a476bf2d97c525
Raa'Shaun Hunter <github@RaaShaunHunter.us>
2022-03-18 18:19:51 -0400

    Reversed the Chassis...again.

--- src/main/java/com/fireteam322/frc/robot/subsystems/Chassis.java
+++ src/main/java/com/fireteam322/frc/robot/subsystems/Chassis.java
@@ -48,8 +48,8 @@ public class Chassis extends SubsystemBase {
 	public Chassis() {
 		super();
 		// Invert the right side motors due to the way they're mounted.
-		m_leftMotors.setInverted(true);
-		m_rightMotors.setInverted(false);
+		m_leftMotors.setInverted(false);
+		m_rightMotors.setInverted(true);
 
 		// Change the motors to "coast" mode
 		brake(false);
