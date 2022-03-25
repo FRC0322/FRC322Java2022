495cfc8c17217c9e73a95c5507e1aa47f217543f
DESKTOP-OHVI0BO\Ashton <ashton.bradley45274@gmail.com>
2022-03-25 00:41:53 -0400

    Formatted code

--- src/main/java/com/fireteam322/frc/robot/inf/RobotContrainerInterface.java
+++ src/main/java/com/fireteam322/frc/robot/inf/RobotContrainerInterface.java
@@ -3,7 +3,9 @@ package com.fireteam322.frc.robot.inf;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public interface RobotContrainerInterface {
-    public void configureButtonBindings();
-    public void chooserSetup();
-    public Command getAutonomousCommand();
+  public void configureButtonBindings();
+
+  public void chooserSetup();
+
+  public Command getAutonomousCommand();
 }
